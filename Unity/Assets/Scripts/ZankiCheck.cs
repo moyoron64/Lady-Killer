cfc936237cbbf1e7d1f576c08f7e3a8944173acf
moyoron64 <daix203440@gamil.com>
2014-09-14 21:55:17 +0900

    zanki

--- Unity/Assets/Scripts/ZankiCheck.cs
+++ Unity/Assets/Scripts/ZankiCheck.cs
@@ -1,15 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ZankiCheck : MonoBehaviour {
-
+public class zankiWindow2 : MonoBehaviour {
+	GUILayer guiLayer;
+	GUITexture zanki1;
+	GUITexture zanki2;
+	GUITexture zanki3;
 	// Use this for initialization
 	void Start () {
+		
+		
+		
+		
+		guiLayer = Camera.main.GetComponent<GUILayer>();
+		
+		
+		GameObject go = GameObject.Find("zanki1");
+		GameObject go2 = GameObject.Find("zanki2");
+		GameObject go3 = GameObject.Find("zanki3");
+		zanki1 = (GUITexture)go.GetComponent(typeof(GUITexture));
+		zanki2 = (GUITexture)go2.GetComponent(typeof(GUITexture));
+		zanki3 = (GUITexture)go3.GetComponent(typeof(GUITexture));
+		
+		
+		
 	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+		if (Player.life == 0) {
+			zanki1.guiTexture.enabled = false;
+		}
+		if (Player.life == 1) {
+			zanki2.guiTexture.enabled = false;
+		}
+		if (Player.life == 2) {
+			zanki3.guiTexture.enabled = false;
+		}
+
 	}
 }
