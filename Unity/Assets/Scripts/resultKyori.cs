267ccf526ad34a86c8cfe28d28fb2ae5f95aded5
moyoron64 <daix203440@gamil.com>
2014-09-17 03:27:59 +0900

    result make

---
+++ Unity/Assets/Scripts/resultKyori.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class resultKyori : MonoBehaviour {
+
+	private static int kyori= 31;
+	public string Text;
+	public int depth =0;
+	public float strWidth=4;
+	public float strLeft=50;
+	public float strTop=60;
+	private float time;
+	public GUIStyle style;
+	
+	// Use this for initialization
+	void Start()
+	{
+		GUI.depth = depth;
+		Text="距離" + kyori+ "mm";
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	
+	void OnGUI()
+	{
+
+			style.fontSize = (int)(Screen.width * strWidth / 100);
+			GUILayout.BeginArea(new Rect(Screen.width * strLeft / 100, Screen.height * strTop / 100,
+			                             style.CalcSize(new GUIContent(Text)).x, style.CalcSize(new GUIContent(Text)).y));
+			
+			GUILayout.Label(Text, style);
+			GUILayout.EndArea();
+			
+			
+	}
+	
+}
