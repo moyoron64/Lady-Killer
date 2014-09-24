8a563772df673799f55f33cc60336ccb79fb2ecf
moyoron64 <daix203440@gamil.com>
2014-09-25 02:50:41 +0900

    commit

--- Unity/Assets/Scripts/CameraControll.cs
+++ Unity/Assets/Scripts/CameraControll.cs
@@ -7,6 +7,7 @@ public class CameraControll : MonoBehaviour {
 	private GameObject goalPosition = null;
 
 	public Vector3 offset;
+	public static float xCameraPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -19,6 +20,9 @@ public class CameraControll : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+
+		xCameraPosition = this.transform.position.x;
+
 		if(this.transform.position.x < this.goalPosition.transform.position.x )return;
 
 		this.transform.position = new Vector3(this.transform.position.x, this.kakuteru.transform.position.y + 3, this.kakuteru.transform.position.z + this.offset.z);
